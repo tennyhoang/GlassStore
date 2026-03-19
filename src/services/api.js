7e8b2b2ac59500cc3f4af652c975c088e8b231d4
tennyhoang <ttuan0147@gmail.com>
2026-03-19 18:51:10 +0700

    Fix API baseURL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const BASE_URL = import.meta.env.VITE_API_URL ?? '/api'
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: BASE_URL,
   headers: { 'Content-Type': 'application/json' },
 })
 
