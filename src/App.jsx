4b483a047315a353a0b4bf107ef7d48a640f5892
tennyhoang <ttuan0147@gmail.com>
2026-03-21 00:40:19 +0700

    Fix: logo link to home, staff pre-orders page

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ import MyReviewsPage    from './pages/customer/MyReviewsPage'
 import ReturnPage       from './pages/customer/ReturnPage'
 import PreOrderPage     from './pages/customer/PreOrderPage'
 import CreatePreOrderPage from './pages/customer/CreatePreOrderPage'
-import StaffPreOrderPage from './pages/staff/StaffPreOrderPage'
+import customerPreOrderPage from './pages/customer/PreOrderPage'
 
 // Staff pages
 import StaffDashboardPage     from './pages/staff/StaffDashboardPage'
